c0f04d9da6a192117c01a30201ad6b213cedeebb
Margo <margo.bozhenova@gmail.com>
2021-01-12 01:41:41 +0300

    Add client folder

---
+++ client/src/index.tsx
@@ -0,0 +1,16 @@
+import { render } from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { Listings } from './sections';
+import './styles/index.css';
+
+const client = new ApolloClient({
+  uri: '/api',
+});
+
+render(
+  <ApolloProvider client={client}>
+    <Listings title='TinyHouse Listings' />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
